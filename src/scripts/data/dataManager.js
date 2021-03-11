6bdb10ace5aa8f26f2fc102fd758c9293e31d2a7
srcrank <salmacrank@gmail.com>
2021-03-11 16:09:00 -0600

    filtering posts

--- src/scripts/data/dataManager.js
+++ src/scripts/data/dataManager.js
@@ -8,16 +8,6 @@ export const getUsers = () => {
     })
 }
 
-export const getPosts = () => {
-
-    return fetch("http://localhost:8088/posts")
-    .then(response => response.json())
-    .then(parsedResponse => {
-        // do something with response here
-        return parsedResponse;
-    })
-}
-
 const loggedInUser = {
 	id: 1,
 	name: "Bryan",
@@ -28,3 +18,20 @@ export const getLoggedInUser = () => {
 	return loggedInUser;
 }
 
+
+let postCollection = [];
+
+export const usePostCollection = () => {
+  //Best practice: we don't want to alter the original state, so
+  //make a copy of it and then return it
+  //The spread operator makes this quick work
+  return [...postCollection];
+}
+export const getPosts = () => {
+  return fetch("http://localhost:8088/posts")
+    .then(response => response.json())
+    .then(parsedResponse => {
+      postCollection = parsedResponse
+      return parsedResponse;
+    })
+}
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -36,6 +36,36 @@ applicationElement.addEventListener("change", event => {
     }
   })
 
+  applicationElement.addEventListener("click", (event) => {
+	
+	if (event.target.id.startsWith("edit")){
+		console.log("post clicked", event.target.id.split("--"))
+		console.log("the id is", event.target.id.split("--")[1])
+	}
+})
+
+applicationElement.addEventListener("change", event => {
+    if (event.target.id === "yearSelection") {
+      const yearAsNumber = parseInt(event.target.value)
+      console.log(`User wants to see posts since ${yearAsNumber}`)
+      //invoke a filter function passing the year as an argument
+      showFilteredPosts(yearAsNumber);
+    }
+  })
+  
+  const showFilteredPosts = (year) => {
+    //get a copy of the post collection
+    const epoch = Date.parse(`01/01/${year}`);
+    //filter the data
+    const filteredData = usePostCollection().filter(singlePost => {
+      if (singlePost.timestamp >= epoch) {
+        return singlePost
+      }
+    })
+    const postElement = document.querySelector(".postList");
+    postElement.innerHTML = PostList(filteredData);
+  }
+
 getUsers()
 .then(data => {
     console.log("User Data", data)
