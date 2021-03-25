2080f696a546507ede37e8991e4100f8e6a93fbe
srcrank <salmacrank@gmail.com>
2021-03-25 12:02:52 -0500

    creating posts

--- src/scripts/data/dataManager.js
+++ src/scripts/data/dataManager.js
@@ -35,3 +35,15 @@ export const getPosts = () => {
       return parsedResponse;
     })
 }
+
+export const createPost = postObj => {
+  return fetch("http://localhost:8088/posts", {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json"
+      },
+      body: JSON.stringify(postObj)
+
+  })
+      .then(response => response.json())
+}
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,8 @@ import { getUsers, getPosts } from "./data/dataManager.js"
 import { PostList } from "./feed/postList.js"
 import { NavBar } from "./nav/navBar.js"
 import { footer } from "./nav/footer.js"
+import { PostEntry } from "./feed/PostEntry.js"
+import { createPost } from "./data/dataManager.js"
 
 
 let postElement = document.querySelector(".postList");
@@ -78,10 +80,45 @@ const showPostList = () => {
     })
 }
 
+applicationElement.addEventListener("click", event => {
+  if (event.target.id === "newPost__cancel") {
+      //clear the input fields
+  }
+})
+
+applicationElement.addEventListener("click", event => {
+  event.preventDefault();
+  if (event.target.id === "newPost__submit") {
+  //collect the input values into an object to post to the DB
+    const title = document.querySelector("input[name='postTitle']").value
+    const url = document.querySelector("input[name='postURL']").value
+    const description = document.querySelector("textarea[name='postDescription']").value
+    //we have not created a user yet - for now, we will hard code `1`.
+    //we can add the current time as well
+    const postObject = {
+        title: title,
+        imageURL: url,
+        description: description,
+        userId: 1,
+        timestamp: Date.now()
+    }
+
+  // be sure to import from the DataManager
+      createPost(postObject)
+  }
+})
+
+const showPostEntry = () => { 
+  //Get a reference to the location on the DOM where the nav will display
+  const entryElement = document.querySelector(".entryForm");
+  entryElement.innerHTML = PostEntry();
+}
+
 const startGiffyGram = () => {
-	showNavBar();
+	  showNavBar();
     showPostList();
     showFooter();
+    showPostEntry();
 }
 // Are you defining the function here or invoking it?
 startGiffyGram();
